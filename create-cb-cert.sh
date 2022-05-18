40038d9c7473de47378135370d13148ce2cd9363
Michael Minichino <mminichino@mac.com>
2022-05-18 17:52:55 -0500

    first commit

--- create-cb-cert.sh
+++ create-cb-cert.sh
@@ -5,11 +5,12 @@ err_exit() {
   exit 1
 }
 
-if [ -z "$1" ]; then
-  err_exit "Usage: $0 domain_name"
+if [ -z "$1" -o -z "$2" ]; then
+  err_exit "Usage: $0 domain_name namespace"
 fi
 
 DOMAIN_NAME=$1
+NAMESPACE=$2
 
 cd easyrsa3 || err_exit "Can not change to directory easyrsa3"
 
@@ -17,7 +18,7 @@ cd easyrsa3 || err_exit "Can not change to directory easyrsa3"
 
 ./easyrsa build-ca nopass
 
-./easyrsa --subject-alt-name='DNS:*.cbos,DNS:*.cbos.cbdemo,DNS:*.cbos.cbdemo.svc,DNS:*.cbos.cbdemo.svc.cluster.local,DNS:cbos-srv,DNS:cbos-srv.cbdemo,DNS:cbos-srv.cbdemo.svc,DNS:*.cbos-srv.cbdemo.svc.cluster.local,DNS:localhost,DNS:*.'"${DOMAIN_NAME}" build-server-full couchbase-server nopass
+./easyrsa --subject-alt-name='DNS:*.cbos,DNS:*.cbos.'"${NAMESPACE}"',DNS:*.cbos.'"${NAMESPACE}"'.svc,DNS:*.cbos.'"${NAMESPACE}"'.svc.cluster.local,DNS:cbos-srv,DNS:cbos-srv.'"${NAMESPACE}"',DNS:cbos-srv.'"${NAMESPACE}"'.svc,DNS:*.cbos-srv.'"${NAMESPACE}"'.svc.cluster.local,DNS:localhost,DNS:*.'"${DOMAIN_NAME}" build-server-full couchbase-server nopass
 
 if [ ! -d "$HOME/tls" ]; then
   mkdir $HOME/tls
